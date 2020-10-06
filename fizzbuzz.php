bea3e5526488ae7b8b4100eb0f028ffc6b339835
XeroGitHub <72323916+XeroGitHub@users.noreply.github.com>
2020-10-06 19:04:22 +0200

    Add files via upload

---
+++ fizzbuzz.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "<div style='display:block'>";
+
+for($i = 1;$i <=100;$i++){ 
+
+   if($i % 3 == 0 && $i % 5 == 0){
+     echo "fizzbuzz ";
+  }else if($i % 3 == 0){
+     echo "fizz ";
+  }else if($i % 5 == 0){
+     echo "buzz ";
+  }else echo $i." ";
+
+}
+
+echo "</div>";
+
+?>
